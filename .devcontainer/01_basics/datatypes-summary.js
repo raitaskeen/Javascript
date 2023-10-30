b211017022afe9ca1f64551e98484a41bd389a5d
Taskeen Haider <137790592+raitaskeen@users.noreply.github.com>
2023-10-30 18:46:51 +0000

    Container config Done

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -6,6 +6,13 @@ const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
 
 
 
