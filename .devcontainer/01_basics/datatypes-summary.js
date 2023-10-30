95c9f56e1a9dc1c2924b5b1b248a940bfca16a51
Taskeen Haider <137790592+raitaskeen@users.noreply.github.com>
2023-10-30 18:40:08 +0000

    Summary Done

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Funtions
