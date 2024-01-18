9b49d3a24fdb600728b982392b1f112d17aaa093
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-18 20:38:30 +0500

    Add math and number operations, update settings.json

--- .devcontainer/01_basics/06_maths_and_numbers.js
+++ .devcontainer/01_basics/06_maths_and_numbers.js
@@ -8,3 +8,15 @@ console.log(score.toString());
 console.log(typeof score);
 console.log(typeof balance);
 console.log(balance.toFixed(6));
+
+const otherScore = 224.23
+console.log(otherScore.toPrecision(4));
+
+const loacaleScore = 123456789
+console.log(loacaleScore.toLocaleString('en-IN'));
+
+// ++++++++++++++ Maths ++++++++++++++++
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(2.4));
+console.log(Math);
