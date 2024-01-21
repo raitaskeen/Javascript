4f91bc7fa731981ec272342ce846fcd576cf75ab
raitaskeen <raitaskeenhaider786@gmail.com>
2024-01-22 00:03:57 +0500

    Add FizzBuzz program

---
+++ programs/fizbuzz.js
@@ -0,0 +1,13 @@
+// FizzBuzz: Print numbers from 1 to 100. For multiples of 3, print "Fizz"; for multiples of 5, print "Buzz"; for both, print "FizzBuzz".
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
