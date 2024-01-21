0faed4f8bddc6a9f55d7ba28caaa274d8cabd2c8
raitaskeen <raitaskeenhaider786@gmail.com>
2024-01-22 00:07:14 +0500

    Add factorial calculation program

---
+++ programs/factorial_calculation.js
@@ -0,0 +1,11 @@
+// Calculate the factorial of a given number.
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log("Factorial of 5:", factorial(5));
